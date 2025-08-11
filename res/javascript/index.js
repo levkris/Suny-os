dad05eaaaa9c1c16e905b3a9a6c07b91f8210aa5
levkris <105733478+levkris@users.noreply.github.com>
2025-08-11 18:13:04 +0200

    fix

--- res/javascript/index.js
+++ res/javascript/index.js
@@ -1,3 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDq91dZ2X9MES8-q9pIctaBONgZwPF7X1o",
+    authDomain: "homeassistant-23079.firebaseapp.com",
+    projectId: "homeassistant-23079",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
 auth.onAuthStateChanged(user => {
   if (!user) {
     window.location.href = "setup.html";
